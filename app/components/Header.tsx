977cbc8f8ab92cb1309fb57b45c7f3b563b7ef31
Zain Fathoni <me@zainf.dev>
2023-11-05 15:14:01 +0800

    feat(theme): implement Naive Dark Mode Switch
    https://www.mattstobbs.com/remix-dark-mode/#1-naive-dark-mode-switch

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,6 +2,7 @@ import { Popover, Transition } from "@headlessui/react";
 import { Link, NavLink, useMatches } from "@remix-run/react";
 import clsx from "clsx";
 import React, { Fragment, useEffect, useRef } from "react";
+import { Theme, useTheme } from "~/contexts/theme";
 import avatarImage from "../images/avatar.jpg";
 import type { ContainerProps } from "./Container";
 import { Container } from "./Container";
@@ -198,7 +199,7 @@ function DesktopNavigation(
   );
 }
 
-function ModeToggle() {
+function ThemeToggle() {
   function disableTransitionsTemporarily() {
     document.documentElement.classList.add("[&_*]:!transition-none");
     self.setTimeout(() => {
@@ -206,26 +207,21 @@ function ModeToggle() {
     }, 0);
   }
 
-  function toggleMode() {
+  const [, setTheme] = useTheme();
+
+  const toggleTheme = () => {
     disableTransitionsTemporarily();
-
-    const darkModeMediaQuery = self.matchMedia("(prefers-color-scheme: dark)");
-    const isSystemDarkMode = darkModeMediaQuery.matches;
-    const isDarkMode = document.documentElement.classList.toggle("dark");
-
-    if (isDarkMode === isSystemDarkMode) {
-      delete self.localStorage.isDarkMode;
-    } else {
-      self.localStorage.isDarkMode = isDarkMode;
-    }
-  }
+    setTheme((prevTheme) =>
+      prevTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT,
+    );
+  };
 
   return (
     <button
       type="button"
       aria-label="Toggle dark mode"
       className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
-      onClick={toggleMode}
+      onClick={toggleTheme}
     >
       <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
       <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
@@ -460,7 +456,7 @@ export function Header() {
               </div>
               <div className="flex justify-end md:flex-1">
                 <div className="pointer-events-auto">
-                  <ModeToggle />
+                  <ThemeToggle />
                 </div>
               </div>
             </div>
--- app/root.tsx
+++ app/root.tsx
@@ -8,14 +8,18 @@ import {
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
 import type { LinksFunction } from "@vercel/remix";
+import clsx from "clsx";
 import { Header } from "./components/Header";
+import { ThemeProvider, useTheme } from "./contexts/theme";
 import styles from "./tailwind.css";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
-export default function App() {
+function App() {
+  const [theme] = useTheme();
+
   return (
-    <html className="h-full antialiased" lang="en">
+    <html className={clsx("h-full antialiased", theme)} lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -38,5 +42,13 @@ export default function App() {
         <Analytics />
       </body>
     </html>
+  );
+}
+
+export default function AppWithProviders() {
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   );
 }
