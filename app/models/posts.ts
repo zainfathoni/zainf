e83d9d08a6b21dbc0850b79db3465b94db66cd3b
Wheeljack <wheeljack@zavi.family>
2026-07-13 09:38:31 +0700

    📝 AI Tools SWE Growth Digest: jul-5-jul-12-2026 (#97)

--- app/models/posts.ts
+++ app/models/posts.ts
@@ -34,6 +34,7 @@ import * as postSeGrowthHidupSehatJun13Jun272026 from "../routes/blog.se-growth-
 import * as postAiToolsSweGrowthJun21Jun282026 from "../routes/blog.ai-tools-swe-growth-jun-21-jun-28-2026.mdx";
 import * as postAiToolsSweGrowthJun28Jul52026 from "../routes/blog.ai-tools-swe-growth-jun-28-jul-5-2026.mdx";
 import * as postSeGrowthHidupSehatJun27Jul112026 from "../routes/blog.se-growth-hidup-sehat-jun-27-jul-11-2026.mdx";
+import * as postAiToolsSweGrowthJul5Jul122026 from "../routes/blog.ai-tools-swe-growth-jul-5-jul-12-2026.mdx";
 export type MdxAttributes = { meta: MdxMetaEntry[] };
 
 export type MdxModule = {
@@ -168,6 +169,7 @@ function sortPostsByDateDescending(posts: Post[]) {
 
 export const getAllPosts = (limit?: number) => {
   const allPosts = [
+    getPostFromMdxModule(postAiToolsSweGrowthJul5Jul122026),
     getPostFromMdxModule(postSeGrowthHidupSehatJun27Jul112026),
     getPostFromMdxModule(postAiToolsSweGrowthJun28Jul52026),
     getPostFromMdxModule(postAiToolsSweGrowthJun21Jun282026),
